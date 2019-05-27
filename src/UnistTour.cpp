0b74e79cdc0f90f48773dcd823bfadab2d9fb08d
Au Le <lethehaiau@gmail.com>
2019-05-27 18:36:02 +0900

    finalize, correct the solution and export result

--- src/UnistTour.cpp
+++ src/UnistTour.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 #define NUM_BLD 7
 
-void loadData(vector<int>& bldCodeMap, vector<int>& bldVisitTime, vector<vector<int> >& travelTime){
+void loadData(map<int, int>& bldIndexMap, vector<int>& bldCodeMap, vector<int>& bldVisitTime, vector<vector<int> >& travelTime){
 	freopen("../data/UnistTour.txt", "r", stdin);
 	for(int i = 0; i < NUM_BLD; ++i){
 		int bldCode;
 		cin >> bldCode;
 		bldCodeMap[i] = bldCode;
+		bldIndexMap[bldCode] = i;
 		cin >> bldVisitTime[i];
 	}
 
@@ -105,11 +106,12 @@ void twoGroupsPlanner(vector<vector<int> >& twoRoutesList, const vector<vector<i
 		}
 	}
 }
-int threeGroupsMinOverlapTime(const vector<vector<int> >& twoRoutesList, const vector<vector<int> >& routeList,
+vector<int> threeGroupsMinOverlapTime(const vector<vector<int> >& twoRoutesList, const vector<vector<int> >& routeList,
 	const vector<vector<int> >& travelTime, const vector<int> bldVisitTime){
+	vector<int> optimalRoute(4, 0);
 	int minOverlapTime = INT_MAX;
 	for(int i = 0; i < twoRoutesList.size(); ++i){
-		if(twoRoutesList[i][2] >= minOverlapTime) return minOverlapTime;
+		if(twoRoutesList[i][2] >= minOverlapTime) return optimalRoute;
 		int firstRoute = twoRoutesList[i][0];
 		int secondRoute = twoRoutesList[i][1];
 		for(int thirdRoute = secondRoute + 1; thirdRoute < routeList.size(); ++thirdRoute){
@@ -118,22 +120,25 @@ int threeGroupsMinOverlapTime(const vector<vector<int> >& twoRoutesList, const v
 			overlapTime += twoRoutesOverlapTime(routeList[secondRoute], routeList[thirdRoute], travelTime, bldVisitTime);
 			minOverlapTime = min(minOverlapTime, overlapTime);
 			if(overlapTime == 0){
-				cout << firstRoute << " " << secondRoute << " " << thirdRoute << endl;
+				optimalRoute[0] = firstRoute;
+				optimalRoute[1] = secondRoute;
+				optimalRoute[2] = thirdRoute;
+				optimalRoute[3] = minOverlapTime;
 			}
 		}
 	}
-	return minOverlapTime;
+	return optimalRoute;
 }
 
-int fourGroupsMinOverlapTime(const vector<vector<int> >& twoRoutesList, const vector<vector<int> >& routeList,
+vector<int> fourGroupsMinOverlapTime(const vector<vector<int> >& twoRoutesList, const vector<vector<int> >& routeList,
 	const vector<vector<int> >& travelTime, const vector<int> bldVisitTime){
+	vector<int> optimalRoute(5, 0);
 	int minOverlapTime = INT_MAX;
 	int count = 0;
 	for(int i = 0; i < twoRoutesList.size() - 1; ++i)
-		for(int j = i; j < twoRoutesList.size(); ++j){
+		for(int j = i + 1; j < twoRoutesList.size(); ++j){
 			if(twoRoutesList[i][2] + twoRoutesList[j][2] >= minOverlapTime){
-				cout << count << endl;
-				return minOverlapTime;
+				break;
 			} 
 			vector<int> fourRoutes;
 			fourRoutes.push_back(twoRoutesList[i][0]);
@@ -145,18 +150,55 @@ int fourGroupsMinOverlapTime(const vector<vector<int> >& twoRoutesList, const ve
 				for(int v = u + 1; v < 4; ++v){
 					overlapTime += twoRoutesOverlapTime(routeList[fourRoutes[u]], routeList[fourRoutes[v]], travelTime, bldVisitTime);
 				}
-			minOverlapTime = min(minOverlapTime, overlapTime);
+			if(overlapTime < minOverlapTime){
+				minOverlapTime = overlapTime;
+				optimalRoute = fourRoutes;
+				optimalRoute[4] = minOverlapTime;
+			}
 			++count;
 		}
-	return minOverlapTime;
+	return optimalRoute;
+}
+
+void printRoute(const vector<int>& bldCodeMap, const vector<int>& route){
+	cout << bldCodeMap[0];
+	for(int i = 0; i < NUM_BLD - 1; ++i){
+		cout << " " << bldCodeMap[route[i]];
+	}
+	cout << "\n";
+}
+
+void loadOriginalPlan(const map<int, int>& bldIndexMap, vector<vector<int> >& originThreeGroupsPlan, vector<vector<int> >& originFourGroupsPlan){
+	ifstream originData("../data/originalPlan.txt");
+	vector<int> route(NUM_BLD - 1, 0);
+	for(int i = 0; i < 3; ++i){
+		for(int j = 0; j < NUM_BLD; ++j){
+			int bldCode;
+			originData >> bldCode;
+			if(j > 0)
+			route[j - 1] = bldIndexMap.find(bldCode)->second;
+		}
+		originThreeGroupsPlan.push_back(route);
+	}
+	for(int i = 0; i < 4; ++i){
+		for(int j = 0; j < NUM_BLD; ++j){
+			int bldCode;
+			originData >> bldCode;
+			if(j > 0)
+			route[j - 1] = bldIndexMap.find(bldCode)->second;
+		}
+		originFourGroupsPlan.push_back(route);
+	}
 }
 
 int main(){
+	freopen("result.txt", "w", stdout);
+	map<int, int> bldIndexMap;
 	vector<int> bldCodeMap(NUM_BLD, 0);
 	vector<int> bldVisitTime(NUM_BLD, 0);
 	vector<vector<int> > travelTime;
 	travelTime.resize(NUM_BLD);
-	loadData(bldCodeMap, bldVisitTime, travelTime);
+	loadData(bldIndexMap, bldCodeMap, bldVisitTime, travelTime);
 	vector<vector<int> > routeList;
 	int initialRoute[] = {1, 2, 3, 4, 5, 6};
 	vector<int> route(initialRoute, initialRoute + NUM_BLD - 1);
@@ -170,13 +212,49 @@ int main(){
 		round(optimalSingleRoute * 1.1), cmpval) - routeList.begin());
 	routeList.resize(pivot);
 
+	//two groups plan
 	vector<vector<int> > twoRoutesList;
 	twoGroupsPlanner(twoRoutesList, routeList, travelTime, bldVisitTime);
 	sort(twoRoutesList.begin(), twoRoutesList.end(), cmp);
+	cout << "Two Groups Plan:\n";
+	cout << "total overlap time: " << twoRoutesList[0][2] << endl;
+	printRoute(bldCodeMap, routeList[twoRoutesList[0][0]]);
+	printRoute(bldCodeMap, routeList[twoRoutesList[0][1]]);
+
+	//three groups plan
+	vector<int> threeGroupsCase = threeGroupsMinOverlapTime(twoRoutesList, routeList, travelTime, bldVisitTime);
+	cout << "Three Groups Plan:\n";
+	cout << "total overlap time: " << threeGroupsCase[3] << endl;
+	for(int i = 0; i < 3; ++i){
+		printRoute(bldCodeMap, routeList[threeGroupsCase[i]]);
+	}
+
+	//four groups plan
+	vector<int> fourGroupsCase = fourGroupsMinOverlapTime(twoRoutesList, routeList, travelTime, bldVisitTime);
+	cout << "Four Groups Plan:\n";
+	cout << "total overlap time: " << fourGroupsCase[4] << endl;
+	for(int i = 0; i < 4; ++i){
+		printRoute(bldCodeMap, routeList[fourGroupsCase[i]]);
+	}
+	vector<vector<int> > originThreeGroupsPlan;
+	vector<vector<int> > originFourGroupsPlan;
+	loadOriginalPlan(bldIndexMap, originThreeGroupsPlan, originFourGroupsPlan);
 	
-	int threeGroupsCase = threeGroupsMinOverlapTime(twoRoutesList, routeList, travelTime, bldVisitTime);
-	cout << threeGroupsCase << endl;
-	int fourGroupsCase = fourGroupsMinOverlapTime(twoRoutesList, routeList, travelTime, bldVisitTime);
-	cout << fourGroupsCase << endl;
+	int overlapTime = 0;
+	for(int i = 0; i < 2; ++i){
+		for(int j = i + 1; j < 3; ++j){
+			overlapTime += twoRoutesOverlapTime(originThreeGroupsPlan[i], originThreeGroupsPlan[j],
+				travelTime, bldVisitTime);
+		}
+	}
+	//cout << overlapTime << endl;
+	overlapTime = 0;
+	for(int i = 0; i < 3; ++i){
+		for(int j = i + 1; j < 4; ++j){
+			overlapTime += twoRoutesOverlapTime(originFourGroupsPlan[i], originFourGroupsPlan[j],
+				travelTime, bldVisitTime);
+		}
+	}
+	//cout << overlapTime << endl;
 	return 0;
 }
